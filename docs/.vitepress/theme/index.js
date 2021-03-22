7ff3efe22240cde2318b2ff730c2a9c83d7bb122
Eduardo San Martin Morote <posva13@gmail.com>
2021-03-22 23:48:05 +0100

    feat: wip

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,14 @@
 import Theme from 'vitepress/theme'
 import './custom.css'
 import './code-theme.css'
+import { createPinia } from '../../../dist/pinia.esm-bundler'
 
-export default {
+/** @type {import('vitepress').Theme} */
+const config = {
   ...Theme,
+  enhanceApp({ app }) {
+    app.use(createPinia())
+  },
 }
+
+export default config
